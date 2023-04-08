7746d667e2978e3b3dea0ebd81a2055d4e184b1b
shephali09 <hiremathshephali9@gmail.com>
2023-04-08 09:57:38 +0530

    Add: Ability to dequeue from the beginning

--- src/com/bridgelabz/stackandqueueproblem/StackAndQueue.java
+++ src/com/bridgelabz/stackandqueueproblem/StackAndQueue.java
@@ -5,27 +5,43 @@ import java.util.*;
 class Queue {
 	private LinkedList linkedlist = new LinkedList();
 
-	public void enqueue(Object obj) {
+	public void enqueu(Object obj) {
 
 		linkedlist.addLast(obj);
 	}
 
+	public Object dequeue()
+	{	
+		return linkedlist.remove();
+	}
+	
+	public boolean isEmpty()
+	{
+		return linkedlist.isEmpty();
+	}
 	public String toString() {
 		return "" + linkedlist;
 
 	}
 }
+
 public class StackAndQueue {
 
 	public static void main(String[] args) {
 
 		Queue queue = new Queue();
 
-		queue.enqueue(56);
-		queue.enqueue(30);
-		queue.enqueue(70);
+		queue.enqueu(56);
+		queue.enqueu(30);
+		queue.enqueu(70);
 
 		System.out.println(queue);
-
+		
+		while(!queue.isEmpty())
+		{
+			queue.dequeue();
+		}
+		System.out.println("Queue is empty : "+queue);
+ 
 	}
 }
